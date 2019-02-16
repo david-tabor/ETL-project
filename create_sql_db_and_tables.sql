3fce337ece2577206eaafb3fb81730e4bc13ed4a
David Tabor <dtabor3@gmail.com>
2019-02-16 13:39:06 -0600

    adding jupyter notebook to write baseball players to sql db

--- create_sql_db_and_tables.sql
+++ create_sql_db_and_tables.sql
@@ -12,6 +12,15 @@ CREATE TABLE colleges (
 );
 
 
+DROP TABLE IF EXISTS players;
+CREATE TABLE players (
+    `player_id` VARCHAR(50) CHARACTER SET utf8,
+	`name_first` VARCHAR(50) CHARACTER SET utf8,
+    `name_last` VARCHAR(50) CHARACTER SET utf8,
+    primary key(player_id)
+);
+
+
 
 
 
