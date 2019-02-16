afdf385cc4364c02bbed8e4c806e47dc540ad98d
David Tabor <dtabor3@gmail.com>
2019-02-16 12:54:32 -0600

    adding sql script to create db and tables

---
+++ create_sql_db_and_tables.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS etl_project;
+CREATE DATABASE etl_project;
+USE etl_project;
+
+DROP TABLE IF EXISTS colleges;
+CREATE TABLE premise (
+    `id` INTEGER AUTO_INCREMENT NOT NULL,
+    `college_name` VARCHAR(50) CHARACTER SET utf8,
+    `applications` INT,
+    `grad_rate` FLOAT,
+    primary key(id)
+);
+
+
+
+
+
